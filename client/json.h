5b1e508fea0bf143b4ba6b5ec0f789bbfdbe49ac
millie-crowther <amelia.g.crowther@gmail.com>
2023-09-15 16:53:53 +0100

    Update json.h

--- client/json.h
+++ client/json.h
@@ -4,14 +4,22 @@
 #include <stdbool.h>
 #include <stdint.h>
 
+typedef enum JSONType {
+    JSON_TYPE_NULL,
+    JSON_TYPE_BOOLEAN,
+    JSON_TYPE_NUMBER,
+    JSON_TYPE_STRING,
+    JSON_TYPE_LIST,
+    JSON_TYPE_OBJECT,
+} JSONType;
+
 typedef struct JSON {
-    union JSONData {
-        bool boolean;
-        int64_t integer;
-        double number;
-        
-    };
     char * start;
 } JSON;
+
+JSONType json_infer_type(const JSON * json);
+bool json_get_bool(const JSON * json);
+int64_t json_get_integer(const JSON * json);
+
 
 #endif
