6e19d124e7f2eea15dd15e5e47ca2ecbd0b98bdd
millie-crowther <amelia.g.crowther@gmail.com>
2023-09-15 17:44:13 +0100

    Update json.h

--- client/json.h
+++ client/json.h
@@ -4,6 +4,8 @@
 #include <stdbool.h>
 #include <stdint.h>
 
+#include "string.h"
+
 typedef enum JSONType {
     JSON_TYPE_NULL,
     JSON_TYPE_BOOLEAN,
@@ -13,23 +15,39 @@ typedef enum JSONType {
     JSON_TYPE_OBJECT,
 } JSONType;
 
-typedef struct JSON {
-    char * start;
-    char * end;
-} JSON;
+typedef struct JSONList {
+    JSON ** elements;
+    uint32_t length;
+} JSONList;
+
+typedef struct JSONKeyPair {
+    String key;
+    JSONObject * object;
+} JSONKeyPair;
+
+typedef struct JSONDictionary {
+    JSONKeyPair * key_pairs;
+    uint32_t length;
+} JSONObject;
+
+typedef struct JSONObject {
+    union {
+        bool boolean;
+        double number;
+        String string;
+        JSONList list;
+        JSONDictionary dictionary;
+    };
+    JSONType type;
+} JSONObject;
 
 typedef struct JSONDocument {
-    char * data;
-    JSON root;
+    JSONObject * root;
 } JSONDocument;
 
-void json_document_create(JSONDocument * document, char * data, uint32_t length);
-void json_document_destroy(JSONDocument * document);
-
-JSONType json_infer_type(const JSON json);
-bool json_get_bool(const JSON json);
-int64_t json_get_integer(const JSON json);
-
+uint32_t json_document_size(const String data);
+void json_document_parse(JSONDocument * document, JSONObject * buffer, const String data);
+JSONObject * json_dictionary_lookup(const JSONDictionary dictionary, const String key);
 
 
 #endif
